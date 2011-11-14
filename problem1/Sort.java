0f0ac67bc8373cdf03bd8ddf642864c76291da27
Masahiro Sakai <masahiro.sakai@gmail.com>
2011-11-15 08:14:45 +0900

    adding problem1/Sort.java

---
+++ problem1/Sort.java
@@ -0,0 +1,41 @@
+class Sort {
+    /*@ requires
+      @   a != null &&
+      @   0 <= i && i <= a.length &&
+      @   0 <= j && j <= a.length;
+      @ assigns a[i], a[j];
+      @ ensures a[i]==\old(a[j]) && a[j]==\old(a[i]);
+      @*/
+    public static void swap(boolean a[], int i, int j) {
+        boolean t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    /*@ requires a != null && 0 <= a.length;
+      @ assigns a[..];
+      @ ensures (\forall integer k; (0 <= k && k < a.length - 1) ==> (a[k]==true ==> a[k+1]==true));
+      @*/    
+    public static void two_way_sort(boolean a[]) {
+        int i = 0;
+        int j = a.length - 1;
+
+        /*@ loop_invariant
+          @   (\forall integer k; 0 <= k && k < i - 1 ==> (a[k]==true ==> a[k+1]==true)) &&
+          @   (\forall integer k; j < k && k < a.length - 1 ==> (a[k]==true ==> a[k+1]==true));
+          @ loop_variant j - i;
+          @*/
+
+        while (i <= j) {
+            if (!a[i])
+                i = i+1;
+            else if (a[j])
+                j = j-1;
+            else {
+                swap(a, i, j);
+                i = i+1;
+                j = j-1;
+            }
+        }
+    }
+}
