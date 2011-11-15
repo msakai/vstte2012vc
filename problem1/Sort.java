753f9f00c6e7b9b12c6d4d78e028199599bef463
Masahiro Sakai <masahiro.sakai@gmail.com>
2011-11-15 22:18:38 +0900

    bug fixes

--- problem1/Sort.java
+++ problem1/Sort.java
@@ -1,8 +1,8 @@
 class Sort {
     /*@ requires
       @   a != null &&
-      @   0 <= i && i <= a.length &&
-      @   0 <= j && j <= a.length;
+      @   0 <= i && i < a.length &&
+      @   0 <= j && j < a.length;
       @ assigns a[i], a[j];
       @ ensures a[i]==\old(a[j]) && a[j]==\old(a[i]);
       @*/
@@ -12,20 +12,20 @@ class Sort {
         a[j] = t;
     }
 
-    /*@ requires a != null && 0 <= a.length;
+    /*@ requires a != null;
       @ assigns a[..];
       @ ensures (\forall integer k; (0 <= k && k < a.length - 1) ==> (a[k]==true ==> a[k+1]==true));
-      @*/    
+      @*/
     public static void two_way_sort(boolean a[]) {
         int i = 0;
         int j = a.length - 1;
 
         /*@ loop_invariant
-          @   (\forall integer k; 0 <= k && k < i - 1 ==> (a[k]==true ==> a[k+1]==true)) &&
-          @   (\forall integer k; j < k && k < a.length - 1 ==> (a[k]==true ==> a[k+1]==true));
+          @   0 <= i && j < a.length &&
+          @   (\forall integer k; 0 <= k && k < i ==> a[k]==false) &&
+          @   (\forall integer k; j < k && k < a.length ==> a[k]==true);
           @ loop_variant j - i;
           @*/
-
         while (i <= j) {
             if (!a[i])
                 i = i+1;
